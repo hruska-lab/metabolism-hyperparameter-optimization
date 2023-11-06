edf48b5ae962e9d2d3ededc396b843c763356486
Lukáš Majer <majer0037@gmail.com>
2023-11-06 20:43:15 +0100

    it4i testing

--- OptunaMorganJazzy.sh
+++ OptunaMorganJazzy.sh
@@ -4,7 +4,7 @@
 #SBATCH --partition qcpu
 #SBATCH --nodes 1
 #SBATCH --ntasks-per-node 128
-#SBATCH --time 24:00:00
+#SBATCH --time 00:30:00
 ml purge
 ml OpenMPI/4.1.4-GCC-11.3.0
 python optuna-it4i.py
